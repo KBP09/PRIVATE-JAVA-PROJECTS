b234985f4543fa26d87fe8dda6150da5a2b6811f
[KBP09] <[kanakbpandey2023@gmail.com]>
2023-09-20 22:17:23 +0530

    [question 3]

---
+++ Excer3.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class Excer3 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        double z = Math.pow(n,a);
+        System.out.println(z);
+    }
+}
