1c7a79fa0e7d981ad272082a0c9ddbb135c5cd94
KBP09 <kanakbpandey2023@gmail.com>
2023-10-06 16:08:01 +0530

    [bubble sort array]

---
+++ bubsort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class bubsort{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        //bubble sort
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        sc.close();
+    }
+}
