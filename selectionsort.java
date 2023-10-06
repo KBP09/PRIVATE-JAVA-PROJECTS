eab20c3a75c398d414fc685dd968f7fb48546332
KBP09 <kanakbpandey2023@gmail.com>
2023-10-06 17:19:28 +0530

    [selection sort of an array]

---
+++ selectionsort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class selectionsort {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<arr.length-1;i++){
+            int smallest = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[smallest]>arr[j]){
+                    smallest = j;
+                }
+            }
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
